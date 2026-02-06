2e05cca5aef5b23a133a9fa00493fef6a430ff29
im borisov <4996131803@mail.ru>
2026-02-06 19:18:55 +0000

    rewrite process_data

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
+MY_SURNAME = 'BORISOV'
+
+MLFLOW_TRACKING_URL = 'http://158.160.2.37:5000'
+
 DATASET_PATH_PATTERN = '/app/data/{split_name}.csv'
 DATASET_NAME = 'scikit-learn/adult-census-income'
 MODEL_FILEPATH = '/app/model.joblib'
--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,50 +1,88 @@
 import os
 import numpy as np
+
+
 import pandas as pd
+import mlflow
 from datasets import load_dataset
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OrdinalEncoder
-
-from constants import DATASET_NAME, DATASET_PATH_PATTERN, TEST_SIZE, RANDOM_STATE
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
+from category_encoders import TargetEncoder
+from constants import MY_SURNAME, MLFLOW_TRACKING_URL, DATASET_NAME, DATASET_PATH_PATTERN, TEST_SIZE, RANDOM_STATE
 from utils import get_logger, load_params
+
+np.random.seed(RANDOM_STATE)
 
 STAGE_NAME = 'process_data'
 
+CAT_ENCODERS = {
+    'ohe': OneHotEncoder(sparse_output=False),
+    'ordial': OrdinalEncoder(),
+    'target': TargetEncoder()
+}
 
 def process_data():
     logger = get_logger(logger_name=STAGE_NAME)
-    params = load_params(stage_name=STAGE_NAME)
+    process_data_params = load_params(stage_name=STAGE_NAME)
 
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URL)
+    mlflow.set_experiment(f"homework_{MY_SURNAME}")
+    
     logger.info('Начали скачивать данные')
     dataset = load_dataset(DATASET_NAME)
     logger.info('Успешно скачали данные!')
 
     logger.info('Делаем предобработку данных')
+
+    num_features = process_data_params['columns']['num_features']
+    cat_features = process_data_params['columns']['cat_features']
+    ignore_features = process_data_params['columns'].get('ignore_features', []) or []
+    
+    target_column = process_data_params['columns']['target']
+    train_columns = list(set(num_features) | set(cat_features) - set(ignore_features))
+
     df = dataset['train'].to_pandas()
-    columns = params['features']
-    target_column = 'income'
-    X, y = df[columns], df[target_column]
-    logger.info(f'    Используемые фичи: {columns}')
+    X, y = df[train_columns], df[target_column]
+    logger.info(f'    Используемые фичи: {train_columns}')
 
-    all_cat_features = [
-        'workclass', 'education', 'marital.status', 'occupation', 'relationship',
-        'race', 'sex', 'native.country',
-    ]
-    cat_features = list(set(columns) & set(all_cat_features))
-    num_features = list(set(columns) - set(all_cat_features))
+    cat_encoder_name = process_data_params['preprocess']['cat_encoder']
+    preprocessor = CAT_ENCODERS[cat_encoder_name]
+    
+    y_transformed = (y == '>50K').astype(int)
 
-    preprocessor = OrdinalEncoder()
-    X_transformed = np.hstack([X[num_features], preprocessor.fit_transform(X[cat_features])])
-    y_transformed: pd.Series = (y == '>50K').astype(int)
+    X_cat_prep = preprocessor.fit_transform(X[cat_features]) if cat_encoder_name != 'target' else preprocessor.fit_transform(X=X[cat_features], y=y_transformed)
+
+    X_transformed = np.hstack([X[num_features], X_cat_prep])
+    
     X_train, X_test, y_train, y_test = train_test_split(
         X_transformed, y_transformed, test_size=TEST_SIZE, random_state=RANDOM_STATE
     )
 
-    # use train_size param to take only train_size rows of train dataset
-    ...
+    n = len(X_train)
+    train_size = process_data_params['subsample_train_size']
+    if train_size < 1.:
+        random_indices = np.random.choice(n, size=int(n*train_size), replace=False)
+
+        X_train = X_train[random_indices]
+        y_train = y_train[random_indices]
+        logger.info(f'    Используем подвыборку train_size={int(n*train_size)}')
+    
     logger.info(f'    Размер тренировочного датасета: {len(y_train)}')
     logger.info(f'    Размер тестового датасета: {len(y_test)}')
 
+    with mlflow.start_run(run_name="data_processing"):
+        mlflow.log_param("dataset_name", process_data_params['dataset'])
+        mlflow.log_param("features", train_columns)
+        mlflow.log_param("train_size_original", n)
+        mlflow.log_param("train_subsample_size", len(y_train))
+        mlflow.log_param("test_size", len(y_test))
+        mlflow.log_param("categorical_features", cat_features)
+        mlflow.log_param("numerical_features", num_features)
+        mlflow.log_param("ignore_features", ignore_features)
+        mlflow.log_param("cat_encoder", cat_encoder_name)
+        mlflow.log_param("target_distribution_train", y_train.value_counts().to_dict())
+        mlflow.log_param("target_distribution_test", y_test.value_counts().to_dict())
+    
     logger.info('Начали сохранять датасеты')
     os.makedirs(os.path.dirname(DATASET_PATH_PATTERN), exist_ok=True)
     for split, split_name in zip(
